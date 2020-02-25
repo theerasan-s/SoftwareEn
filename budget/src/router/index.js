3452c143f9916fa1d24ab8c83387f633568e0322
Theerasan Suanse <theerasan@kkumail.com>
2020-02-25 20:46:04 +0700

    jabbbb

--- budget/src/router/index.js
+++ budget/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import test from '@/components/test'
 import hello from '@/components/HelloWorld'
+import home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -11,7 +12,7 @@ export default new Router({
         {
             path: '/',
             name: 'home',
-            component: test
+            component: home
           },
           {
             path: '/s',
