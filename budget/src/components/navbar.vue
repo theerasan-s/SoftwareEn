845f1b950f9278e79fd1a2d4d369865f24955a47
Theerasan Suanse <theerasan@kkumail.com>
2020-03-03 16:10:54 +0700

    navbar fix2

--- budget/src/components/navbar.vue
+++ budget/src/components/navbar.vue
@@ -3,11 +3,11 @@
     <v-app-bar color="#3E3C3C" dense dark>
       <v-toolbar-title style="margin-top: 0px">ระบบติดตามการใช้จ่ายตามแผนงบประมาณ</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn color="#3E3C3C" style="margin-top: 0px">หน้าหลัก</v-btn>
+      <a style="color:white" class="mr-5">หน้าหลัก</a>
         <div class="text-center">
         <v-menu offset-y>
           <template v-slot:activator="{ on }">
-            <v-btn color="#3E3C3C" dark v-on="on" style="margin-top: 0px">รายละเอียดโครงการ</v-btn>
+            <a v-on="on" style="color:white" class="mr-3">รายละเอียดโครงการ</a>
           </template>
           <v-list>
             <v-list-item v-for="(item) in departmentselect" :key="item">
