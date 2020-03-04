b4e8956d1cdea7be21c97f7f5a089285c207b396
Khanittha-git <53257532+Khanittha-git@users.noreply.github.com>
2020-03-05 00:04:21 +0700

    Edit UI Login

--- budget/src/components/Login.vue
+++ budget/src/components/Login.vue
@@ -1,49 +1,78 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-col cols="12">
-        <v-img :src="require('../assets/loginicon.png')" class="my-3" contain height="350" />
-      </v-col>
-    </v-container>
-    <v-card class="cardadd" color="#F1F1F1">
-      <v-row justify="center">
-        <v-col cols="12" sm="8">
-          <v-text-field label="Username" outlined background-color="#FFFFFF"></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-          <v-col cols="12" sm="8">
-          <v-text-field
-          background-color="#FFFFFF"
-            outlined
-            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="show1 ? 'text' : 'password'"
-            label="Password"
-            @click:append="show1 = !show1"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row justify="center"><v-btn style="margin:0px; background-color:#AF281A; color:#FFFF;">Login</v-btn></v-row>
-    </v-card>
+  <v-app >
+     
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+      
+       
+    
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+ <v-img :src="require('../assets/loginicon.png')" class="my-3" height="200px" />
+            <v-card class="elevation-12">
+              
+              <v-toolbar
+                color="#AF281A"
+                dark
+                flat
+                
+              >
+                <v-toolbar-title>Login Budjet System</v-toolbar-title>
+                <v-spacer />
+               
+              </v-toolbar>
+              
+              <div style="padding:16px;"><v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                   
+                    type="text"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="#AF281A" dark
+                flat>Login</v-btn>
+              </v-card-actions></div>
+              
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
+
 <script>
-export default {
-  data() {
-    return {
-      show1: false,
-      password: "Password"
-    };
+  export default {
+    props: {
+      source: String,
+    },
   }
-};
 </script>
+
 <style>
-button{
-    margin: 0px;
-}
-.cardadd {
-  padding: 20px;
-  margin-left: 450px;
-  margin-right: 450px;
-}
+
+
 </style>
